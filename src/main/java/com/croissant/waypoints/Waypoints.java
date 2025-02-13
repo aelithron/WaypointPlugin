21a5a09edb8699568300b5e096e98bc14c479210
Aelithron <aelithron@gmail.com>
2025-02-13 01:59:55 +0000

    Fix formatting and file saving Here we go! :3
    Signed-off-by: Aelithron <aelithron@gmail.com>

--- src/main/java/com/croissant/waypoints/Waypoints.java
+++ src/main/java/com/croissant/waypoints/Waypoints.java
@@ -3,12 +3,22 @@ package com.croissant.waypoints;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Waypoints extends JavaPlugin {
+    // Files
+    YamlConfiguration waypointStore;
+    // Actual "File" objects (used for reloading)
+    private File waypointStoreFile;
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new WaypointListener(this), this);
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        // Data Storage
+        waypointStoreFile = new File(getDataFolder(), "waypoints.yml");
+        if (!waypointStoreFile.exists()) {
+            saveResource("waypoints.yml", false);
+        }
+        reloadDataStore();
         CoreTools.getInstance().setPlugin(this); // Extra initialization
         CoreTools.getInstance().checkForUpdates();
     }
@@ -16,5 +26,17 @@ public final class Waypoints extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    
+    public void reloadDataStore() {
+        dataStore = YamlConfiguration.loadConfiguration(waypointStoreFile);
+    }
+
+    public void saveDataStore() {
+        try {
+            dataStore.save(new File(getDataFolder(), "waypoints.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
